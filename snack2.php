a712718717ef2e63051d7486840140ab96db638d
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-29 16:56:16 +0100

    creates an array containing the key of the main array, get the single element inside the cycle using the array keys

--- snack2.php
+++ snack2.php
@@ -1,4 +1,63 @@
 <?php 
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+// var_dump($posts);
+
+//creo un array che contiene ogni chiave per ogni elemento dell'array post (così da recuperare i singoli elementi)
+$arraykeys = array_keys($posts);
+// var_dump($arraykeys);
+
+$elementoUno = $arraykeys[0];
+// var_dump($posts[$elementoUno])
+
+
+for($i = 0; $i < count($posts); $i++){
+    //recupero il singolo elemento dell'array di oggetti $post che ha come key $arraykeys[$i] 
+    $singlePost = $posts[$arraykeys[$i]];
+
+    var_dump($singlePost);
+
+    /* for($j = 0; $j < count($singlePost); $j++){
+
+    } */
+}
 
 ?>
 
@@ -11,6 +70,8 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    ?>
     
 </body>
 </html>
